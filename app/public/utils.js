c2a64f544a351d5b13b009cee2ad376b237c49b5
Jonathan Girard Viau <girardvjonathan@gmail.com>
2017-02-04 21:15:00 -0500

    things

---
+++ app/public/utils.js
@@ -0,0 +1,11 @@
+/**
+ * Created by john on 2/4/17.
+ */
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+}
