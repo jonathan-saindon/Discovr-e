675928f8f63d1f2c79ec0d0419089fc5bb833610
Jonathan Girard Viau <girardvjonathan@gmail.com>
2017-02-04 20:42:54 -0500

    montreal

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hackatown-2017
+##TODO
+* add image and add url on click sur le marker on peut override et mettre un pop-up
+* html current location
--- app/public/gmap.js
+++ app/public/gmap.js
@@ -1,28 +1,31 @@
 var map;
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
 function initMap() {
     console.log("hola123123");
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: 45.5016889, lng: -73.56725599999999},
+        zoom: 14
     });
-   // addMarker({ lat: 12.97, lng: 77.59 }, map);
+   addMarker({ lat: 45.5016889, lng: -73.56725599999999 }, map);
 
 
 }
 
 // Adds a marker to the map.
-function addMarker(location, map) {
+function addMarker(location, map, name) {
     // Add the marker at the clicked location, and add the next-available label
     // from the array of alphabetical characters.
     var marker = new google.maps.Marker({
         position: location,
-        label: labels[labelIndex++ % labels.length],
+        label: name,
         map: map
     });
 }
 
 
+
 // $(function() {
-    initMap();
+initMap();
 // });
 // google.maps.event.addDomListener(window, 'load', initMap);
